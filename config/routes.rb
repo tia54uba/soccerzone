0092a2b72f6a8983994d74c6ad6f27b7d31af5fb
tia54uba <miyagawa19951018@gmail.com>
2022-03-09 17:47:00 +0000

    add ユーザーマイページ ユーザー編集画面 ユーザー情報更新

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'homes/about',as: 'about'
   resources :posts, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ test/controllers/show_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class ShowControllerTest < ActionDispatch::IntegrationTest
+  test "should get edit" do
+    get show_edit_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get show_index_url
+    assert_response :success
+  end
+end
