86c250e9dd3876d70674fd95a828c292c0a4cbd0
tia54uba <miyagawa19951018@gmail.com>
2022-03-10 11:45:32 +0000

    add コメント機能

---
+++ app/controllers/post_comments_controller.rb
@@ -0,0 +1,26 @@
+class PostCommentsController < ApplicationController
+
+  def new
+    @post_comment = PostComment.new
+    @post = Post.find(params[:post_id])
+  end
+
+  def create
+    post = Post.find(params[:post_id])
+    comment = current_user.post_comments.new(post_comments_params)
+    comment.post_id = post.id
+    comment.save
+    redirect_to post_path(post)
+  end
+  
+  def destroy
+    PostComment.find_by(id: params[:id], post_id: params[:post_id]).destroy
+    redirect_to request.referer
+  end
+
+    private
+
+    def post_comments_params
+      params.require(:post_comment).permit(:comment)
+    end
+end
