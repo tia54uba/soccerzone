4f62276b2c7a021de7a982c774a632b4867f56ac
tia54uba <miyagawa19951018@gmail.com>
2022-04-16 10:03:44 +0000

    update batch

--- lib/batch/data_reset.rb
+++ lib/batch/data_reset.rb
@@ -1,7 +1,7 @@
 class Batch::DataReset
   def self.data_reset
     # 投稿を全て削除
-    Post.delete_all
+    Post.where("created_at < ?", 30.days.ago.beginning_of_day).delete_all
     p "投稿を全て削除しました"
   end
 end
