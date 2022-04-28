f77af42dfca52ed808518177a23178aa6a4a3ebb
tia54uba <miyagawa19951018@gmail.com>
2022-04-28 04:14:21 +0000

    add favorite index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,12 @@ class UsersController < ApplicationController
       render :edit
     end
   end
+  
+  def favorites
+    @user = User.find(params[:id])
+    favorites= Favorite.where(user_id: @user.id).pluck(:post_id)
+    @favorite_posts = Post.find(favorites)
+  end
 
   private
 
