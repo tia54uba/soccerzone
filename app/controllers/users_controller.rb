d34749a4f6a03c2d78e53f75f4db5bb70ed0c4f4
tia54uba <miyagawa19951018@gmail.com>
2022-04-28 11:02:23 +0000

    update favorite

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
   def favorites
     @user = User.find(params[:id])
     favorites= Favorite.where(user_id: @user.id).pluck(:post_id)
-    @favorite_posts = Post.find(favorites)
+    @favorite_posts = Post.where(id: favorites)
   end
 
   private
